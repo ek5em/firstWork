4c1f8f0014188db737ef2a076e991c84a85d566c
ek5em <pro-100-dim@mail.ru>
2022-11-23 16:15:03 +0400

    fixed all

--- js/main.js
+++ js/main.js
@@ -1,64 +1,74 @@
-window.onload = () => {
-    const graph2d = new Graph2d();
+function shotHandler() {
+    var x = document.getElementById('x').value - 0;
+    var y = document.getElementById('y').value - 0;
+    document.getElementById('shotResult').innerHTML = 'Результат: ' + shot(x, y, 0);
+    x.value = '';
+    y.value = '';
+}
+function shotsHandler() {
+    var min = document.getElementById('min').value - 0,
+        max = document.getElementById('max').value - 0,
+        count = document.getElementById('shotCount').value - 0;
+    document.getElementById('shotsResult').innerHTML = 'Результат: ' + shots(min, max, count, 1)
+}
 
-    new UI({
-        changeColor,
-        changeWidth,
-        changeA,
-        changeB,
-        switchCheckBox,
-        addFunction,
-        delFunction,
-        createObjectFunc,
-    });
 
-    function changeWidth(num, width) {
-        graph2d.funcs[num].width = width;
+function rootsHandler() {
+    var a = document.getElementById('a').value-0,
+        b = document.getElementById('b').value-0,
+        c = document.getElementById('c').value-0,
+        d = document.getElementById('d').value-0,
+        e = document.getElementById('e').value-0,
+        roots = getRoots(
+            a ? a : 0,
+            b ? b : 0,
+            c ? c : 0,
+            d ? d : 0,
+            e ? e : 0,
+        );
+        document.getElementById('rootsResult').innerHTML = `Уравнение ${textRootsHandler(roots[1])} = 0 ${rootsCounter(roots[0])}  ${roots[0]}`
+}
+
+
+function menuHandler(event) {
+    var contents = document.querySelectorAll('.content-item');
+    menuItems = document.querySelectorAll('.menu-item');
+    for (var i = 0; i < contents.length; i++) {
+        contents[i].classList.add('hide');
+        menuItems[i].classList.remove('activated')
     }
+    var buttonId = event.target.dataset.button;
+    document.getElementById(buttonId).classList.add('activated');
 
-    function changeColor(num, color) {
-        graph2d.funcs[num].color = color;
-    }
+    var contentId = event.target.dataset.content;
+    document.getElementById(contentId).classList.remove('hide');
+}
 
-    function addFunction(num, f) {
-        graph2d.funcs[num].f = f;
-    }
+window.onload = function () {
+    var shotButton = document.getElementById('shot');
+    shotButton.addEventListener('click', shotHandler);
 
-    function changeA(num, value) {
-        graph2d.funcs[num].a = value - 0;
-    }
+    var rootsButton = document.getElementById('calcRoots');
+    rootsButton.addEventListener('click', rootsHandler);
 
-    function changeB(num, value) {
-        graph2d.funcs[num].b = value - 0;
-    }
+    var shotsClear = document.getElementById('shotsClear');
+    shotsClear.addEventListener('click', clearShots);
 
-    function switchCheckBox(num) {
-        graph2d.funcs[num].derivative = !graph2d.funcs[num].derivative;
-    }
+    var shotClear = document.getElementById('shotClear');
+    shotClear.addEventListener('click', clearShot);
 
-    function delFunction(num) {
-        graph2d.funcs[num] = null;
-    }
+    var shotsButton = document.getElementById('shots');
+    shotsButton.addEventListener('click', shotsHandler);
 
-    function createObjectFunc(num) {
-        graph2d.funcs[num] = {
-            f: null,
-            color: 'black',
-            width: 2,
-            a: 0,
-            b: 0,
-        }
-    }
+    var menuButtons = document.querySelectorAll('.menu-item');
+    for (var i = 0; i < menuButtons.length; i++) {
+        menuButtons[i].addEventListener('click', menuHandler);
+    };
 
-    function getZero(f, a, b, eps = 0.0001) {
-        if (f(a) * f(b) > 0) return null;
-        if (f(a) === 0) return a;
-        if (f(b) === 0) return b;
-        if (Math.abs(f(b) - f(a) <= eps)) return (a + b) / 2;
-        const half = (a + b) / 2;
-        if (f(a) * f(half) <= 0) return getZero(f, a, half, eps)
-        if (f(b) * f(half) <= 0) return getZero(f, half, b, eps)
-        else return null;
-    }
+    var beginButton = document.querySelector('.beginButton');
+    beginButton.addEventListener('click', beginQuest);
 
+    document.querySelector('.continueButton').addEventListener('click', continueQuest);
+
+    new Graph2d;
 }
